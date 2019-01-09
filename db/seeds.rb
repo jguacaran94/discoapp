a35cd46bf909bb847705473ff72b95a4a9cf52a1
Jorge Guacarán <guacaranjorge94@gmail.com>
2019-01-09 00:48:50 -0400

    Fix.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Artist.create(name: "Metallica", description: "Metallica es una banda de thrash metal estadounidense que se origina en Los Ángeles, pero con base en San Francisco desde febrero de 1983.")
+
+Artist.create(name: "Megadeth", description: "Megadeth es una banda estadounidense de thrash metal, formada en Los Ángeles, California. Fue creada en 1983 por Dave Mustaine (vocalista y guitarrista), después de que fuera expulsado de Metallica, donde ocupaba el puesto de guitarrista principal.")
+
+Artist.create(name: "Slayer", description: "Slayer es una banda de thrash metal estadounidense fundada en 1981 por los guitarristas Kerry King y Jeff Hanneman en Huntington Park, California.")
+
+Artist.create(name: "Anthrax", description: "Anthrax es una banda estadounidense de thrash metal, formada en Nueva York en 1981, por el guitarrista Scott Ian y el bajista Dan Lilker.")
+
+Artist.create(name: "Rammstein", description: "Rammstein es una banda alemana de metal industrial formada en 1994 por los músicos Till Lindemann, Richard Z. Kruspe, Oliver Riedel, Paul Landers, Christian Lorenz y Christoph Schneider.")
+
+Artist.create(name: "Gojira", description: "Gojira es una banda Francesa de death metal y groove metal formada en 1996 en Bayona, Francia.")
+
+Lp.create(name: "Kill 'Em All", year: "1983", genre: "Thrash Metal")
