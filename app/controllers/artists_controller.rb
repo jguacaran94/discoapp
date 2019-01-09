e90e237595cac0b88bc91c336a9e2961e733b9a0
Jorge Guacarán <guacaranjorge94@gmail.com>
2019-01-08 23:52:24 -0400

    Se creó CRUD para Artistas.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,53 @@
+class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy, :lps]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def edit
+  end
+
+  def lps
+    @lps = Lp.where(artist_id: params[:id])
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      redirect_to @artist, notice: 'Artist was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @artist.update(artist_params)
+      redirect_to @artist, notice: 'Artist was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @artist.destroy
+    redirect_to artists_url, notice: 'Artist was successfully destroyed.'
+  end
+
+  private
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
+
+    def artist_params
+      params.require(:artist).permit(:name, :description)
+    end
+end
